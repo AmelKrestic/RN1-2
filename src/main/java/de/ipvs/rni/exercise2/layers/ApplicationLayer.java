a8e61949658ed6fadef5d4ec69dca76e5ec30990
Amel Krestic <st177355@stud.uni-stuttgart.de>
2024-05-25 10:34:46 +0200

    Commit

---
+++ src/main/java/de/ipvs/rni/exercise2/layers/ApplicationLayer.java
@@ -0,0 +1,183 @@
+package de.ipvs.rni.exercise2.layers;
+
+import de.ipvs.rni.exercise2.common.Application;
+import de.ipvs.rni.exercise2.common.ProcessEvents;
+import de.ipvs.rni.exercise2.common.Utils;
+import java.io.Serializable;
+import java.util.ArrayDeque;
+import java.util.Iterator;
+
+/**
+ * This class represents the application layer.
+ * 
+ * An Application can bind to the application layer for network communication.
+ * The application layer provides the application with three methods for message
+ * communication: 'sendMessage', 'hasMessage', and 'popMessage'.
+ *
+ */
+public class ApplicationLayer implements ProcessEvents {
+
+	public final static byte END = (byte) 0xAE;
+
+	// Input Queue/Buffer, to store messages from the application.
+	private ArrayDeque<Serializable> fromUpper = new ArrayDeque<>();
+	// Ouput Buffer, where the appliaction layer forwards an received object/message
+	// to the application.
+	private ArrayDeque<Object> toUpper = new ArrayDeque<>();
+	// Input Buffer, where the appliaction layer receives new bytes from its lower
+	// layer.
+	private ArrayDeque<Byte> fromLower;
+	// Output Buffer, where the appliaction layer forwards its data to next lower
+	// layer.
+	private ArrayDeque<Byte> toLower;
+	// Buffer for aggregating bytes before object creation.
+	private ArrayDeque<Byte> messageBuffer = new ArrayDeque<>();
+
+	/**
+	 * Bind an app to the application layer.
+	 * 
+	 * @param app An application which should be bound to this layer
+	 */
+	public void bind(Application app) {
+		app.setApplicationLayer(this);
+	}
+
+	/**
+	 * Transform an object into an byte array/buffer/queue. Conversion via Utils
+	 * class, adding END flag at the end
+	 * 
+	 * @param obj Any object which implements the Serializable interface
+	 * @return
+	 */
+	public ArrayDeque<Byte> transformMessageForSending(Serializable obj) {
+		// TODO
+		ArrayDeque<Byte> temp = Utils.objToBytes(obj);
+		temp.add(END);
+		return temp;
+	}
+
+	/**
+	 * Transforms the input bytes back into an object to be handed over to the next
+	 * layer. Removal of END flag.
+	 * 
+	 * @param byteInput
+	 * @return the object reconstructed
+	 */
+	public Object transformMessageForReceiving(ArrayDeque<Byte> byteInput) {
+		byteInput.removeLast();
+		return Utils.bytesToObj(byteInput);
+
+	}
+
+	/**
+	 * Forward bytes to next lower layer
+	 * 
+	 * @param byteBuffer Source Byte Buffer
+	 * @param nextLayer  Destination Byte Buffer
+	 */
+	public void sendMessageBytes(ArrayDeque<Byte> byteBuffer, ArrayDeque<Byte> nextLayer) {
+		nextLayer.addAll(byteBuffer);
+	}
+
+	/**
+	 * Handles communication between layers. First loop converts objects from
+	 * application queue to byte queues and adds them to toLower. Second loop reads
+	 * out byte queue from lower layer and separates them into objects with END
+	 * character.
+	 */
+	@Override
+	public void process() {
+
+		while (!fromUpper.isEmpty()) {
+			Serializable obj = fromUpper.poll();
+			ArrayDeque<Byte> msg = transformMessageForSending(obj);
+			toLower.addAll(msg);
+		}
+		while (!fromLower.isEmpty()) {
+			ArrayDeque<Byte> msg = messageBuffer;
+			Byte b = null;
+			while (!fromLower.isEmpty()) {
+				b = fromLower.poll();
+				msg.add(b);
+				if (b == END) {
+					break;
+				}
+			}
+			if (b != END) {
+				messageBuffer = msg;
+				break;
+			}
+			Object obj = transformMessageForReceiving(msg);
+			toUpper.add(obj);
+			messageBuffer = new ArrayDeque<>();
+		}
+
+	}
+
+	/**
+	 * Send an object which implements the interface Serializable.
+	 * 
+	 * @param obj
+	 */
+	public void sendMessage(Serializable obj) {
+		fromUpper.offer(obj);
+	}
+
+	/**
+	 * Probe if we received an object/message.
+	 * 
+	 * @return
+	 */
+	public boolean hasMessage() {
+		return !toUpper.isEmpty();
+	}
+
+	/**
+	 * Get new object/message and remove it from the application layer queue/buffer.
+	 * 
+	 * @return
+	 */
+	public Object pollMessage() {
+		return toUpper.poll();
+	}
+
+	/**
+	 * Check if byteBuffer has an end delimiter.
+	 * 
+	 * This method assumes that the byte buffer has stuffed bytes .
+	 * 
+	 * @param byteBuffer
+	 * @return
+	 */
+	public ArrayDeque<Serializable> getFromUpper() {
+		return fromUpper;
+	}
+
+	public void setFromUpper(ArrayDeque<Serializable> fromUpper) {
+		this.fromUpper = fromUpper;
+	}
+
+	public ArrayDeque<Object> getToUpper() {
+		return toUpper;
+	}
+
+	public void setToUpper(ArrayDeque<Object> toUpper) {
+		this.toUpper = toUpper;
+	}
+
+	public ArrayDeque<Byte> getFromLower() {
+		return fromLower;
+	}
+
+	public void setFromLower(ArrayDeque<Byte> fromLower) {
+		this.fromLower = fromLower;
+	}
+
+	public ArrayDeque<Byte> getToLower() {
+		return toLower;
+	}
+
+	public void setToLower(ArrayDeque<Byte> toLower) {
+		this.toLower = toLower;
+	}
+}
---
+++ src/main/java/de/ipvs/rni/exercise2/layers/DataLinkLayer.java
@@ -0,0 +1,186 @@
+package de.ipvs.rni.exercise2.layers;
+
+import de.ipvs.rni.exercise2.common.*;
+import de.ipvs.rni.exercise2.packets.*;
+
+import java.util.ArrayDeque;
+import java.util.Iterator;
+
+public class DataLinkLayer implements ProcessEvents
+{
+    private static final int MTU = 1500;
+    private static final int RETRANSMISSION_TIMEOUT = 4;
+    public static final boolean CRC_IMPLEMENTED = true;
+
+    private ArrayDeque<Byte> fromUpper = new ArrayDeque<>();
+    private ArrayDeque<Byte> toUpper = new ArrayDeque<>();
+    // The in-flight queue contains all elements which are not ACKed yet
+    private ArrayDeque<Frame> inFlight = new ArrayDeque<>();
+    // The retransmissionQueue contains the frames which need to be retransmitted
+    public ArrayDeque<Frame> retransmissionQueue = new ArrayDeque<>();
+    // The retransmissionTimeouts contains the timeouts for the frames in the in-flight queue (same order)
+    private ArrayDeque<Integer> retransmissionTimeouts = new ArrayDeque<>();
+    private ArrayDeque<Frame> fromLower;
+    private ArrayDeque<Frame> toLower;
+
+    private int seqNo = 0;
+    private int nextSeqNo = 0;
+    private int availWindow = 5;
+    private int nextAck = -1;
+    private int curTime = 0;
+
+    public DataLinkLayer()
+    {
+        nextAck = seqNo;
+    }
+
+    public void bind(ApplicationLayer appLayer){
+       appLayer.setFromLower(toUpper);
+       appLayer.setToLower(fromUpper);
+    }
+    
+    /**
+     * Converts Byte Queue to byte array and constructs Frame from it.
+     * @param bytesToSent
+     * @return
+     */
+    public Frame createNextFrame(ArrayDeque<Byte> bytesToSent)
+    {
+           //TODO
+    	int size=bytesToSent.size();
+    	size=size>MTU-4-4-8?MTU-4-4-8:size;
+    	byte[] bytes=new byte[size];
+    	int i=0;
+    	while(i<size) {
+    		bytes[i]=bytesToSent.poll().byteValue();
+    		i++;
+    	}
+  		Frame f=new Frame(seqNo++,bytes, bytes.length);
+        return f;
+    }
+
+    @Override
+    public void process()
+    {
+        curTime++;
+	    // TODO
+        //Recieve
+        while(!fromLower.isEmpty()) {
+        	Frame f=fromLower.poll();
+        	if(f.checkIntegrity()) {
+        		if(f.isAck()) {
+        			int ackNr=f.getAckNo();
+        			while(ackNr>=nextAck) {
+        				nextAck++;
+        				availWindow++;
+        				inFlight.poll();
+        				retransmissionQueue.poll();
+        			}
+        		}else {
+        			byte[] temp=f.getPayload();
+        			for(byte b:temp) {
+        				toUpper.add(b);
+        			}
+        			int toAck=f.getSeqNo();
+        			Frame ack=new Frame(toAck);
+        			toLower.add(ack);
+         		}
+        	}
+        	
+        }
+        //Re-send
+        if(!inFlight.isEmpty()) {
+        	if((curTime-retransmissionTimeouts.peek())>RETRANSMISSION_TIMEOUT) {
+        		toLower.addAll(inFlight);
+        		retransmissionTimeouts.clear();
+        		for(Frame f:inFlight) {
+        			retransmissionTimeouts.add(curTime);
+        		}
+        	}
+        }
+        
+        //Send
+        while(!fromUpper.isEmpty()&&availWindow>0) {
+        	Frame f=createNextFrame(fromUpper);
+        	toLower.add(f);
+        	inFlight.add(f);
+        	retransmissionTimeouts.add(curTime);
+        	availWindow--;
+        }
+	}		  
+
+
+    //////////////////////////////////////////////////
+    // Getter and Setter for testing purposes only! //
+    //////////////////////////////////////////////////
+
+    public int getSeqNo()
+    {
+        return seqNo;
+    }
+
+    public ArrayDeque<Byte> getFromUpper()
+    {
+        return fromUpper;
+    }
+
+    public void setFromUpper(ArrayDeque<Byte> fromUpper)
+    {
+        this.fromUpper = fromUpper;
+    }
+
+    public ArrayDeque<Byte> getToUpper()
+    {
+        return toUpper;
+    }
+
+    public void setToUpper(ArrayDeque<Byte> toUpper)
+    {
+        this.toUpper = toUpper;
+    }
+
+    public ArrayDeque<Frame> getInFlight()
+    {
+        return inFlight;
+    }
+
+    public void setInFlight(ArrayDeque<Frame> inFlight)
+    {
+        this.inFlight = inFlight;
+    }
+
+    public ArrayDeque<Frame> getFromLower()
+    {
+        return fromLower;
+    }
+
+    public void setFromLower(ArrayDeque<Frame> fromLower)
+    {
+        this.fromLower = fromLower;
+    }
+
+    public ArrayDeque<Frame> getToLower()
+    {
+        return toLower;
+    }
+
+    public void setToLower(ArrayDeque<Frame> toLower)
+    {
+        this.toLower = toLower;
+    }
+
+    public void setSeqNo(int seqNo)
+    {
+        this.seqNo = seqNo;
+    }
+
+    public int getAvailWindow()
+    {
+        return availWindow;
+    }
+
+    public void setAvailWindow(int availWindow)
+    {
+        this.availWindow = availWindow;
+    }
+}
